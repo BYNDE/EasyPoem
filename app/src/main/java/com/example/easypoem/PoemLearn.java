3eed53daeef9ffff1d2bdcdbdf444580a6643520
dvd-denis <pikuldenis9@gmail.com>
2021-05-08 12:46:29 +0600

    d

--- app/src/main/java/com/example/easypoem/PoemLearn.java
+++ app/src/main/java/com/example/easypoem/PoemLearn.java
@@ -1,66 +1,39 @@
 package com.example.easypoem;
 
-import android.content.Intent;
-import android.os.Build;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Random;
 
-public class PoemLearn extends AppCompatActivity implements View.OnClickListener {
-    String[] mass;
-    Button[] buttons;
-    Button BTN_send;
-    String word;
-    int wordid;
-    TextView T_text;
-    TextView T_title;
-    EditText ET;
-    int waring = 0;
+public class PoemLearn extends AppCompatActivity {
 
-    int countPhase = 3;
-    int phase = 0;
-    LinearLayout LL_Start;
-    LinearLayout LL_Learn;
-    LinearLayout LL_Learn2;
-    LinearLayout LL_Finish;
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poem_learn);
 
-        String default_title = getIntent().getExtras().getString("title");
-        String default_text = getIntent().getExtras().getString("text");
+        String title = getIntent().getExtras().getString("title");
+        String text = getIntent().getExtras().getString("text");
 
-        T_title = findViewById(R.id.TV_title);
-        T_text = findViewById(R.id.TV_text);
-        T_text.setText(getIntent().getExtras().getString("text"));
+        TextView T_title = findViewById(R.id.TV_title);
+        TextView T_text = findViewById(R.id.TV_text);
+        T_text.setText(text);
 
-        LL_Start = findViewById(R.id.LL_Start);
-        LL_Learn = findViewById(R.id.LL_Learn);
-        LL_Learn2 = findViewById(R.id.LL_Learn2);
-        LL_Finish = findViewById(R.id.LL_Finish);
-
-        ET = findViewById(R.id.ET);
+        LinearLayout LL_Start = findViewById(R.id.LL_Start);
+        LinearLayout LL_Learn = findViewById(R.id.LL_Learn);
 
         // LL_Learn
         Button BTN_variant1 = findViewById(R.id.BTN_variant1);
-        BTN_variant1.setOnClickListener(this);
         Button BTN_variant2 = findViewById(R.id.BTN_variant2);
-        BTN_variant2.setOnClickListener(this);
         Button BTN_variant3 = findViewById(R.id.BTN_variant3);
+<<<<<<< HEAD
         BTN_variant3.setOnClickListener(this);
         buttons = new Button[]{BTN_variant1, BTN_variant2, BTN_variant3};
 
@@ -68,37 +41,26 @@ public class PoemLearn extends AppCompatActivity implements View.OnClickListener
         BTN_send = findViewById(R.id.BTN_send);
         BTN_send.setTag("ET");
         BTN_send.setOnClickListener(this);
+=======
+>>>>>>> parent of 1761f3a (f)
 
         // LL_Start
         findViewById(R.id.BTN_next).setOnClickListener(v -> {
-            if (countPhase <= 5) {
-                LL_Start.setVisibility(View.INVISIBLE);
-                LL_Learn.setVisibility(View.VISIBLE);
-                LL_Learn2.setVisibility(View.INVISIBLE);
-                LL_Finish.setVisibility(View.INVISIBLE);
-            } else {
-                LL_Start.setVisibility(View.INVISIBLE);
-                LL_Learn.setVisibility(View.INVISIBLE);
-                LL_Learn2.setVisibility(View.VISIBLE);
-                LL_Finish.setVisibility(View.INVISIBLE);
-            }
-            T_title.setText(default_title);
+            LL_Start.setVisibility(View.INVISIBLE);
+            LL_Learn.setVisibility(View.VISIBLE);
 
-            mass = default_text.split(" ");
+            String[] mass = text.split(" ");
+            Random random = new Random();
+            int var1 = random.nextInt(mass.length);
+            int var2 = random.nextInt(mass.length);
+            int var3 = random.nextInt(mass.length);
 
-            SelectWord();
+            BTN_variant1.setText(mass[var1]);
+            BTN_variant2.setText(mass[var2]);
+            BTN_variant3.setText(mass[var3]);
         });
-        findViewById(R.id.BTN_back).setOnClickListener(v -> {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        });
-        //LL_Finish
-        findViewById(R.id.BTN_finish).setOnClickListener(v -> {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        });
-    }
 
+<<<<<<< HEAD
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onClick(View v) {
@@ -181,5 +143,11 @@ public class PoemLearn extends AppCompatActivity implements View.OnClickListener
 
         }
         T_text.setText(String.join(" ", mass));
+=======
+
+
+
+>>>>>>> parent of 1761f3a (f)
     }
+
 }
