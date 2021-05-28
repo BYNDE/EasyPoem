7b0cb29377ee4fc9d179785decfd328b69dc20f1
dvde123 <pikuldenis9@gmail.com>
2021-05-28 18:22:34 +0600

    d

--- app/src/main/java/com/example/easypoem/drag_and_drop.java
+++ app/src/main/java/com/example/easypoem/drag_and_drop.java
@@ -1,23 +1,25 @@
 package com.example.easypoem;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
-
 import android.content.ClipData;
-import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.DragEvent;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.easypoem.learn.Text;
 import com.google.android.flexbox.AlignItems;
 import com.google.android.flexbox.FlexDirection;
+import com.google.android.flexbox.FlexboxLayout;
 import com.google.android.flexbox.FlexboxLayoutManager;
 import com.google.android.flexbox.JustifyContent;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 public class drag_and_drop extends AppCompatActivity implements word_item_adapter.OnLongClickItem{
     TextView word_textview;
@@ -27,33 +29,82 @@ public class drag_and_drop extends AppCompatActivity implements word_item_adapte
     View entered_word;
     ArrayList<String> words = new ArrayList<>();
 
+    FlexboxLayout flexboxLayout;
+
+     String defText = "— Скажи-ка, дядя, ведь не даром\n" +
+            "Москва, спаленная пожаром,\n" +
+            "Французу отдана?\n" +
+            "Ведь были ж схватки боевые,\n" +
+            "Да, говорят, еще какие!\n" +
+            "Недаром помнит вся Россия\n" +
+            "Про день Бородина!\n" +
+            "\n" +
+            "— Да, были люди в наше время,\n" +
+            "Не то, что нынешнее племя:\n" +
+            "Богатыри — не вы!\n" +
+            "Плохая им досталась доля:\n" +
+            "Немногие вернулись с поля…\n" +
+            "Не будь на то господня воля,\n" +
+            "Не отдали б Москвы!\n" +
+            "\n" +
+            "Мы долго молча отступали,\n" +
+            "Досадно было, боя ждали,\n" +
+            "Ворчали старики:\n" +
+            "«Что ж мы? на зимние квартиры?\n" +
+            "Не смеют, что ли, командиры\n" +
+            "Чужие изорвать мундиры\n" +
+            "О русские штыки?»\n" +
+            "\n" +
+            "И вот нашли большое поле:\n" +
+            "Есть разгуляться где на воле!\n" +
+            "Построили редут.\n" +
+            "У наших ушки на макушке!\n" +
+            "Чуть утро осветило пушки\n" +
+            "И леса синие верхушки —\n";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drag_and_drop);
         words_recycler = findViewById(R.id.words_recycler);
+        flexboxLayout = findViewById(R.id.flex_layout);
+
+        Text text = new Text(defText);
+
+        int selectidWord = text.randomWord();
+        text.selectWord(selectidWord);
+        for (int i = 0; i < text.words.length; i++) {
+            if (i != selectidWord) {
+                TextView textView = new TextView(this);
+                textView.setText(text.words[i]);
+                textView.setGravity(Gravity.CENTER);
+                textView.setTextSize(20);
+                flexboxLayout.addView(textView);
+            } else {
+                TextView textView = new TextView(this);
+                textView.setText("         ");
+                textView.setGravity(Gravity.CENTER);
+                textView.setTextSize(20);
+                textView.setBackground(Drawable.createFromPath("@drawable/border_word"));
+                textView.setId(R.id.word);
+                flexboxLayout.addView(textView);
+            }
+        }
 
         FlexboxLayoutManager manager = new FlexboxLayoutManager(this);
         manager.setFlexDirection(FlexDirection.ROW);
         manager.setJustifyContent(JustifyContent.CENTER);
         manager.setAlignItems(AlignItems.CENTER);
 
-        words.add("яблоко");
-        words.add("привет");
-        words.add("улица");
-        words.add("дом");
-        words.add("телефон");
-        words.add("лампа");
-
-
+        words.add(text.selectedWord);
+        words.add(text.words[text.randomWord()]);
+        words.add(text.words[text.randomWord()]);
+        Collections.shuffle(words);
 
         adapter = new word_item_adapter(this, words,drag_and_drop.this);
         words_recycler.setLayoutManager(manager);
 
         words_recycler.setAdapter(adapter);
-
-
-
 
         word_textview = findViewById(R.id.word);
 
---
+++ app/src/main/java/com/example/easypoem/learn/Text.java
@@ -0,0 +1,88 @@
+package com.example.easypoem.learn;
+
+import java.util.Random;
+
+public class Text {
+    public String text;
+    public String[] words;
+    public String selectedWord;
+    public String[] lines;
+    public String selectedLine = "Не отдали б Москвы!";
+
+
+    public Text(String text) {
+        this.text = text;
+        words = text.split("\\b");
+        lines = text.split("\\n+");
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void getText(String text) {
+        this.text = text;
+    }
+
+    public int randomWord() {
+        Random random = new Random();
+        int r = random.nextInt(words.length);
+
+        boolean var = true;
+
+        while (var) {
+            if ((words[r].matches("([\n ,.:;?!«»…—-])*")) || (words[r].equals(selectedWord))) {
+                r = random.nextInt(words.length);
+//                System.out.println(words[r]);
+            } else {
+                var = false;
+            }
+        }
+
+//        selectedWord = words[r];
+        return r;
+    }
+
+    public int randomLine() {
+        Random random = new Random();
+        int r = random.nextInt(lines.length);
+
+        boolean var = true;
+
+        while (var) {
+            if (lines[r].equals(selectedLine)) {
+                r = random.nextInt(lines.length);
+//                System.out.println(words[r]);
+            } else {
+                var = false;
+            }
+        }
+
+//        selectedLine = lines[r];
+        return r;
+    }
+
+    public void setWord(int i, String word) {
+        words[i] = word;
+    }
+
+    public String getWord(int i) {
+        return words[i];
+    }
+
+    public void selectWord(int i) {
+        selectedWord = words[i];
+    }
+
+    public void setLine(int i, String line) {
+        lines[i] = line;
+    }
+
+    public String getLine(int i) {
+        return lines[i];
+    }
+
+    public void selectLine(int i) {
+        selectedLine = lines[i];
+    }
+}
