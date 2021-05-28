4111ab12c615ff077194961148bc2ddb293d5ddb
dvde123 <pikuldenis9@gmail.com>
2021-05-28 17:23:41 +0600

    Revert "Merge branch 'main' of https://github.com/BYNDE/EasyPoem into main"
    This reverts commit 771c65118bf2801eee6b8086accd496a064d3fc9, reversing
    changes made to d005d8e58735e9de0f9c38be2e7b0ee7242059d7.

---
+++ app/src/main/java/com/example/easypoem/words_item.java
@@ -0,0 +1,12 @@
+package com.example.easypoem;
+
+public class words_item {
+    public String word;
+
+    public words_item() {}
+
+    public words_item(String word){
+        this.word=word;
+    }
+
+}
